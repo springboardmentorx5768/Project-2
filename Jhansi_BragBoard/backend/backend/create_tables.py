8dc53399656b1b2dbbfbd549207edd07f4a78f4f
Addala Jhansi Sri Venkata Muthya Mani Sri <jhansiaddala5@gmail.com>
2025-10-13 20:04:31 +0530

    Removed duplicate nested backend and frontend folders

--- Jhansi_BragBoard/backend/backend/create_tables.py
+++
@@ -1,8 +0,0 @@
-# create_tables.py
-from database import Base, engine
-import models
-
-print("Creating tables in the database...")
-Base.metadata.create_all(bind=engine)
-print("Tables created.")
-
